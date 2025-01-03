aa4390dcf3ba602867a990178154ac23553e573c
MargaritaFilipovich <153985075+MargaritaFilipovich@users.noreply.github.com>
2025-01-03 18:24:14 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//1
+let result = 5 + 5 + '5';
+console.log(result); 
+console.log(typeof result); 
+
+//2
+let email = 'margaruta@gmail.com';
+let lengthOfEmail = email.length;
+console.log(email.includes('@'));
+console.log(email.length);
+
+//3
+let a = "My";
+let b = "name";
+let c = "is";
+let fullName = `${a} ${b} ${c} Viktor`;
+console.log(fullName); 
+
+//4
+let userName = "Олександро"; 
+let payment = 300; 
+//в мене тут не виходить
+
+
